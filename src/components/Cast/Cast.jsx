58af1a39d8a9fa4dcb63292ec4832dbb3b055848
Serhii-Buha <serhii.buga@gmail.com>
2023-05-27 20:03:20 +0300

    add component Cast

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,31 @@
+import axiosGetMovieCast from 'api/axiosGetMovieCast';
+import { FilmCast } from 'components/FilmCast/FilmCast';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Cast = () => {
+  const { movieId } = useParams();
+
+  const [credits, setCredits] = useState({});
+
+  const creditsCast = credits.cast;
+
+  useEffect(() => {
+    if (!movieId) return;
+
+    axiosGetMovieCast(movieId)
+      .then(response => {
+        if (response) {
+          setCredits(response);
+        } else {
+          setCredits({});
+        }
+      })
+
+      .catch(err => console.error(err));
+  }, [movieId]);
+
+  return <div>{creditsCast && <FilmCast creditsCast={creditsCast} />}</div>;
+};
+
+export default Cast;
