3b26d7a4600bd69b941fe13a27e0e0446e9bd14e
Serhii-Buha <serhii.buga@gmail.com>
2023-05-27 20:09:24 +0300

    add component Reviews

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,39 @@
+import { axiosGetMovieReviews } from 'api/axiosGetMovieReviews';
+import { FilmReviews } from 'components/FilmReviews/FilmReviews';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const Reviews = () => {
+  const { movieId } = useParams();
+
+  const [reviews, setReviews] = useState({});
+
+  const resultsReviews = reviews.results;
+
+  useEffect(() => {
+    if (!movieId) return;
+
+    axiosGetMovieReviews(movieId)
+      .then(response => {
+        if (response) {
+          setReviews(response);
+        } else {
+          setReviews({});
+        }
+      })
+
+      .catch(err => console.error(err));
+  }, [movieId]);
+
+  return (
+    <div>
+      {resultsReviews?.length > 0 ? (
+        <FilmReviews resultsReviews={resultsReviews} />
+      ) : (
+        <p>We don't have any reviews for this movie.</p>
+      )}
+    </div>
+  );
+};
+
+export default Reviews;
