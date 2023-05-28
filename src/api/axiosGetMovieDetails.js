0ed6e32140cc81df614188cbcfe50fcee333f5a0
Serhii-Buha <serhii.buga@gmail.com>
2023-05-28 18:32:53 +0300

    Editing api queries

--- src/api/axiosGetMovieDetails.js
+++ src/api/axiosGetMovieDetails.js
@@ -1,29 +1,3 @@
-// import axios from 'axios';
-
-// const API_KEY =
-//   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTZkZmVjNWY0Yjg2YzcwZGEzYTg5NjY5NDI2OTIxOSIsInN1YiI6IjY0NmIxYzk3MzNhMzc2MDBlNjdiNjdlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.soetT_SCyteJCJ0Ce-svzkrEAEk0bSxMGfT7ftm-M1M';
-
-// const axiosOptions = {
-//   baseURL: 'https://api.themoviedb.org/',
-//   headers: {
-//     Authorization: `Bearer ${API_KEY}`,
-//   },
-// };
-
-// const axiosGetMovieDetails = async movieId => {
-//   try {
-//     const response = await axios.get(
-//       `3/movie/${movieId}?language=en-US`,
-//       axiosOptions
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default axiosGetMovieDetails;
-
 import axios from 'axios';
 
 const API_KEY = '5f2a66e63fa9a8139a0b7e8b9aba27ca';
